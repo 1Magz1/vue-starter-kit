7cd767d82c2f16fa9abeea20fe06b343c2c70ecb
1Magz1 <46316981+1Magz1@users.noreply.github.com>
2023-07-23 16:08:44 +0600

    add test component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts" name="App">
+import Counter from "./components/Counter.vue";
+</script>
+
 <template>
   <div class="test">
     App
+    <Counter/>
   </div>
 </template>
---
+++ src/components/Counter.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts" name="Counter">
+import {ref} from "vue";
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="counter">
+    <div class="counter__value">
+      {{count}}
+    </div>
+    <div class="counter__btn">
+      <button @click="count++">+</button>
+      <button @click="count--">-</button>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.counter {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  border-radius: 5px;
+  .counter__value {
+    font-size: 20px;
+  }
+  .counter__btn {
+    display: flex;
+    gap: 10px;
+  }
+}
+</style>
